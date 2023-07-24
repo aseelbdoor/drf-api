1b90ff98593790dd2fa33f6b32a3241a1675e546
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-07-24 17:23:35 +0000

    docker

---
+++ snacks/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from .models import Snack
+from rest_framework.generics import ListAPIView,RetrieveAPIView,ListCreateAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from .serializers import SnackSerializer
+
+class SnackListView(ListCreateAPIView):
+    queryset=Snack.objects.all()
+    serializer_class=SnackSerializer
+
+class SnackDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=Snack.objects.all()
+    serializer_class=SnackSerializer
